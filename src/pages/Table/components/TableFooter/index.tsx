f4cf74bb44bd8901cdbfd36f397f012a8b96f68d
Andrei Kasianok <andrei.kosianok@gmail.com>
2023-10-26 19:37:41 +0200

    Prettier changes

--- src/pages/Table/components/TableFooter/index.tsx
+++ src/pages/Table/components/TableFooter/index.tsx
@@ -14,11 +14,14 @@ const TableFooter = ({ data }: IFooterProps) => {
   return (
     <Flex justify="space-between" align="center">
       <Text>Total</Text>
-      <Text style={{ marginRight: '24px' }}>
-        {data.reduce((acc, item) =>
-          item.type === EIncomeType.INCOME
-            ? acc + item.amount
-            : acc - item.amount, 0)}
+      <Text style={{ marginRight: "24px" }}>
+        {data.reduce(
+          (acc, item) =>
+            item.type === EIncomeType.INCOME
+              ? acc + item.amount
+              : acc - item.amount,
+          0,
+        )}
       </Text>
     </Flex>
   );
